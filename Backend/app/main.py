b3361a1c3455cff0b5c7a8639aae0fd88c2b0708
susanluisa <susanluisacoari@gmail.com>
2025-12-09 21:12:16 -0500

    feat: set cors  configuration

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,23 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import routes_users, routes_auth
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="API Riesgos Ocupacionales")
+
+app = FastAPI(title="API Severidad accidentes Ocupacionales")
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
